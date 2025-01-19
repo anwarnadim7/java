52e0db8425e055165be4374ec9e640898afee4c5
Nadim Anwar <anwarnadim3@gmail.com>
2025-01-19 18:56:14 +0530

    New
    new question added till 14

---
+++ src/Q14.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+// Program to calculate y based on the given formula
+public class Q14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input the value of X
+        System.out.print("Enter the value of X: ");
+        double x = sc.nextDouble();
+
+        // Calculate the numerator and denominator
+        double numerator = Math.pow(x, 6) + Math.pow(x, 4) + Math.pow(x, 2) + 10;
+        double denominator = Math.pow(x, 5) + Math.pow(x, 3) + x;
+
+        // Calculate y
+        if (denominator != 0) {
+            double y = numerator / denominator;
+            System.out.printf("The value of Y is: %.2f%n", y);
+        } else {
+            System.out.println("The denominator is zero, so Y is undefined.");
+        }
+    }
+}
